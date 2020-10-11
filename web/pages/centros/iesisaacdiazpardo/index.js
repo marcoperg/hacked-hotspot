3fd87099bcda5eadc1d8f842d42f0cfd6cbee4be
Marco Pérez González <marcopg4@gmail.com>
2020-10-12 00:19:47 +0200

    Implementent websocket to change spoofwebside

--- web/pages/centros/iesisaacdiazpardo/index.js
+++ web/pages/centros/iesisaacdiazpardo/index.js
@@ -1,24 +1,48 @@
 import iesisaacdiazpardoHtml from '../../../assets/iesisaacdiazpardoHtml';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
+import { client, w3cwebsocket as W3CWebSocket } from 'websocket';
 
 export default function Home() {
-	const [upsideDown, setUpsideDown] = useState(false);
-	const [rotation, setRotation] = useState(false);
+	const ws = useRef(null);
 
-	function toogleUpsideDown() {
-		setUpsideDown(!upsideDown);
-	}
-	function toogleRotation() {
-		setRotation(!rotation);
-	}
+	const [rotation, setRotation] = useState(false);
+	const [upsideDown, setUpsideDown] = useState(false);
+
+	useEffect(() => {
+		ws.current = new W3CWebSocket('ws://127.0.0.1:4000/', 'echo-protocol');
+
+		ws.current.onerror = () => {
+			console.log('Connection Error');
+		};
+
+		ws.current.onopen = () => {
+			console.log('WebSocket Client Connected');
+		};
+
+		ws.current.onclose = () => {
+			console.log('echo-protocol Client Closed');
+		};
+	}, []);
+
+	useEffect(() => {
+		ws.current.onmessage = ({ data }) => {
+			switch (data) {
+				case 'upsideDown':
+					setUpsideDown(!upsideDown);
+					break;
+
+				case 'rotation':
+					setRotation(!rotation);
+					break;
+			}
+		};
+	}, [rotation, upsideDown]);
 
 	const className = classNames({ upsideDown: upsideDown, rotation: rotation });
 
 	return (
 		<div>
-			<button onClick={toogleUpsideDown}>Upside-down</button>
-			<button onClick={toogleRotation}>Rotation</button>
 			<div className={className} dangerouslySetInnerHTML={{ __html: iesisaacdiazpardoHtml }}></div>;
 		</div>
 	);
